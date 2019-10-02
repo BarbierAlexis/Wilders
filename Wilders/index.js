55db633c9bb7fd6c5e47ccee5e269fabe0ee56ff
Alexis Barbier <barbier-alexis@hotmail.fr>
2019-10-02 10:40:55 +0200

    first commit

---
+++ Wilders/index.js
@@ -0,0 +1,6 @@
+const chalk = require ('chalk');
+
+console.log(chalk.blue.bgYellow.bold('Melanie'));
+console.log(chalk.magentaBright.bgWhite.bold('Violette'));
+console.log(chalk.green.bgRed.bold('Maxence'));
+console.log(chalk.yellow.bgCyan.bold('Baptiste'));
